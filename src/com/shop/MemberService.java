7d4c3ce7be9c937d28cb0ec4508a33eb658308bd
phk <phk2143@naver.com>
2022-01-19 13:54:31 +0900

    commit4

--- src/com/shop/MemberService.java
+++ src/com/shop/MemberService.java
@@ -1,5 +1,9 @@
 package com.shop;
 
 public class MemberService {
+	private MemberDao memberDao;
+	public MemberService() {
+		memberDao=new MemberDao();
+	}
 
 }
--- src/com/shop/ProductService.java
+++ src/com/shop/ProductService.java
@@ -2,4 +2,10 @@ package com.shop;
 
 public class ProductService {
 
+	private ProductDao productDao;
+
+	public ProductService() {
+		productDao = new ProductDao();
+	}
+
 }
